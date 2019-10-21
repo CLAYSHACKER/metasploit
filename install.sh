8731892c39f28e9153956b8a6a452db1c0e009b4
CLAYS HACKER <37822373+CLAYSHACKER@users.noreply.github.com>
2019-10-21 16:06:47 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,43 @@
+pkg install autoconf -y
+pkg install bison -y
+pkg install clang -y
+pkg install coreutils -y
+pkg install curl -y
+pkg install findutils -y
+pkg install git -y
+pkg install apr -y
+pkg install apr-util -y
+pkg install libffi -y
+pkg install libgmp -y
+pkg install libpcap -y
+pkg install postgresql -y
+pkg install readline -y
+pkg install libsqlite -y
+pkg install openssl -y
+pkg install libtool -y
+pkg install libxml2 -y
+pkg install libxslt -y
+pkg install ncurses -y
+pkg install pkg-config -y
+pkg install wget -y
+pkg install ruby -y
+pkg install libgrpc -y
+pkg install termux-tools -y
+pkg install ncurses -y
+pkg install ncurses-util -y
+pkg install libsodium -y
+pkg install termux-exec -y
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+pkg install openssh -y
+pkg install php -y
+pkg install nano -y
+pkg install vim -y
+pkg install mc -y
+pkg install figlet -y
+
+clear
+
+figlet CLAYS  
+
